eb56fcb393b0ae37e879d2c1f1484c2ccca09d8f
Lakshay Bansal <lakshaybansal1905@gmail.com>
2025-05-03 22:14:07 +0530

    added migration to add new fields to bookings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 const express=require('express');
 const bodyParser=require("body-parser");
 const app=express();
+const apiRoutes=require('./routes/index');
 
 const {PORT}=require('./config/serverConfig');
+
+const db=require('./models/index')
 
 const setUpAndStartServer=()=>{
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
+    app.use('/api',apiRoutes);
+
     app.listen(PORT,()=>{
         console.log(`Server started at PORT ${PORT}`);
-    })
+
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});
+        }
+    });
 }
 
 setUpAndStartServer(); 
